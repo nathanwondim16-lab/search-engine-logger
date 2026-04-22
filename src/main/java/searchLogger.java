98dbb6f8cf205f45a51200b5c728c24c6864d30c
Nathan Wondim <nathanwondim16@gmail.com>
2026-04-21 21:45:17 -0700

    Completed search logger exercise

--- src/main/java/searchLogger.java
+++ src/main/java/searchLogger.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 public class searchLogger {
+    static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-d");
+        DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("h:mm:ss a");
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("logs.txt"));
+            writer.write(LocalDate.now().format(dateFormat) + "  " + LocalTime.now().format(timeFormat) + " launch\n");
+            while(true) {
+                System.out.print("Enter a search term (X to exit): ");
+                String search = scanner.nextLine().strip();
+                writer.write(LocalDate.now().format(dateFormat) + "  " + LocalTime.now().format(timeFormat) + " ");
+                if(search.equalsIgnoreCase("x")) {
+                    writer.write("exit\n");
+                    break;
+                } else {
+                    writer.write("search : " + search + "\n");
+                }
+            }
+            writer.close();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
 }
